6c4d99434de2fbf205aeaac7c87d885178f01022
Ana Skarica <ana.skarica.as@gmail.com>
2022-07-11 20:51:25 +0200

    scroll radi, css treba popravak

--- src/Components/Oglasi.jsx
+++ src/Components/Oglasi.jsx
@@ -3,7 +3,48 @@ import Oglas from "./Oglas";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Oglasi = ({ setSelectedFilter, filtOglasi }) => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   const filterHandler = (e) => {
     setSelectedFilter(e.target.value);
   };
@@ -15,6 +56,30 @@ const Oglasi = ({ setSelectedFilter, filtOglasi }) => {
       // console.log("prazno");
     }
   }, [filtOglasi]);
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      // <div
+      //   className={className}
+      //   style={{ ...style, display: "block", background: "red" }}
+      //   onClick={onClick}
+      // />
+
+      <div className="arrow arr-right" onClick={onClick}>
+        <FontAwesomeIcon icon={faAngleRight} />
+      </div>
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { onClick } = props;
+    return (
+      <div className="arrow arr-left" onClick={onClick}>
+        <FontAwesomeIcon icon={faAngleLeft} />
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -28,29 +93,25 @@ const Oglasi = ({ setSelectedFilter, filtOglasi }) => {
           </select>
           <h2>Najnoviji oglasi</h2>
         </div>
-
         <div className="oglasi">
-          <div className="arrow arr-left">
-            <FontAwesomeIcon icon={faAngleLeft} />
-          </div>
           <div className="oglasi-map">
-            {filtOglasi.map(
-              (
-                oglas,
-                index // goes through filtered array
-              ) => (
-                <Oglas
-                  name={oglas.name}
-                  category={oglas.category}
-                  price={oglas.price}
-                  key={index}
-                  photoUrl={oglas.photoUrl}
-                />
-              )
-            )}
-          </div>
-          <div className="arrow arr-right">
-            <FontAwesomeIcon icon={faAngleRight} />
+            <Slider {...settings}>
+              {/* we can very easily take the object and spread its properties down to a component as individual prop values using the object spread operator {...myPropObject}.  */}{" "}
+              {filtOglasi.map(
+                (
+                  oglas,
+                  index // goes through filtered array
+                ) => (
+                  <Oglas
+                    name={oglas.name}
+                    category={oglas.category}
+                    price={oglas.price}
+                    key={index}
+                    photoUrl={oglas.photoUrl}
+                  />
+                )
+              )}
+            </Slider>
           </div>
         </div>
       </div>
