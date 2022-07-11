92dc0ce63fdc8f3dcf858d0fbadf0ce7c41dcf2b
Ana Skarica <ana.skarica.as@gmail.com>
2022-07-11 19:51:35 +0200

    dodani oglasi

--- src/App.js
+++ src/App.js
@@ -16,11 +16,39 @@ function App() {
         "https://mcn-images.bauersecure.com/wp-images/4552/bmw-g310gs-40-years.jpg",
     },
     {
-      name: "Tele",
+      name: "Cow",
       category: "Animal",
       price: "1000€",
       photoUrl:
         "https://i0.hippopx.com/photos/1014/166/417/calf-cow-nature-beef-preview.jpg",
+    },
+    {
+      name: "Chicken",
+      category: "Animal",
+      price: "5€",
+      photoUrl:
+        "https://images.unsplash.com/photo-1620136717591-841a4da27e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      name: "Audi R8",
+      category: "Car",
+      price: "1000000€",
+      photoUrl:
+        "https://www.slobodenpecat.mk/wp-content/uploads/2019/08/promo3993.jpg",
+    },
+    {
+      name: "Labrador puppies",
+      category: "Animal",
+      price: "200€",
+      photoUrl:
+        "https://www.sisak.info/wp-content/uploads/2020/05/funniest-cutest-labrador-puppies-2-funny-puppy-videos-2020.jpg",
+    },
+    {
+      name: "Mercedes AMG GT",
+      category: "Car",
+      price: "2000000€",
+      photoUrl:
+        "https://www.driveteam.hr/wp-content/uploads/Mercedes-AMG-GT-Black-series-cijena-narudzba-4.jpg",
     },
   ]);
 
